d6d4bacdae10634611ccfadb0a06455489f3e8c8
GinoGalotti <gino.galotti@gmail.com>
2019-05-18 13:57:45 +0200

    Start mars rovers kata

---
+++ MarsRover/mars_rover.py
@@ -0,0 +1,124 @@
+# Your Task
+# You’re part of the team that explores Mars by sending remotely controlled vehicles to the surface of the planet. Develop an API that translates the commands sent from earth to instructions that are understood by the rover.
+
+# Requirements
+# You are given the initial starting point (x,y) of a rover and the direction (N,S,E,W) it is facing.
+# The rover receives a character array of commands.
+# Implement commands that move the rover forward/backward (f,b).
+# Implement commands that turn the rover left/right (l,r).
+# Implement wrapping from one edge of the grid to another. (planets are spheres after all)
+# Implement obstacle detection before each move to a new square. If a given sequence of commands encounters an obstacle
+
+# You are given a starting point (x,y), and a direction (N,S,E.W).
+# The rover receives a collection of commands, implammant a command that will move the rover (f,b). And methods that will turn the rover
+
+from enum import Enum
+
+
+class Direction(Enum):
+    N = 0
+    E = 1
+    S = 2
+    W = 3
+
+
+class Move(Enum):
+    FORWARD = "f"
+    BACK = "b"
+
+
+class Turn(Enum):
+    LEFT = "l"
+    RIGHT = "r"
+
+# Create exception when initing the wrong parameters of direction/position/landscape
+# TODO: Create another class to accept a line of commands!
+class Rover:
+
+    def __init__(self, landscape, position=(0, 0), direction=Direction.N):
+        self.position = position
+        self.direction = direction
+        self.landscape = landscape
+
+    # To avoid having to check direcitons, we will have forward/backgrounds methods
+    def move(self, direction):
+
+        if (direction == Move.FORWARD):
+            switcher = {
+                Direction.N: (self.position[0]-1, self.position[1]),
+                Direction.W: (self.position[0],   self.position[1]-1),
+                Direction.S: (self.position[0]+1, self.position[1]),
+                Direction.E: (self.position[0],   self.position[1]+1)
+            }
+        else:
+            switcher = {
+                Direction.N: (self.position[0]+1, self.position[1]),
+                Direction.W: (self.position[0],   self.position[1]+1),
+                Direction.S: (self.position[0]-1, self.position[1]),
+                Direction.E: (self.position[0],   self.position[1]-1)
+            }
+
+        new_position = switcher.get(self.direction)
+
+        if self.can_move(new_position):
+            previous_position = self.position
+            self.position = new_position
+
+            if self.is_there_obstacle():
+                self.position = previous_position
+                return False
+            else:
+                return True
+
+        return False
+
+    def move_forward(self):
+        return self.move(Move.FORWARD)
+
+    def move_backwards(self):
+        return self.move(Move.BACK)
+
+    # As we don't have switch statements, and I don't want to use enums as integers, I'll do turn left and right
+    def turn_left(self):
+        switcher = {
+            Direction.N: Direction.W,
+            Direction.W: Direction.S,
+            Direction.S: Direction.E,
+            Direction.E: Direction.N
+        }
+
+        self.direction = switcher.get(self.direction)
+
+    def turn_right(self):
+        switcher = {
+            Direction.N: Direction.E,
+            Direction.E: Direction.S,
+            Direction.S: Direction.W,
+            Direction.W: Direction.N
+        }
+
+        self.direction = switcher.get(self.direction)
+
+    # For testing purposes
+    def can_move(self, position):
+        if (position[0] < 0) or (position[1] < 0):
+            return False
+
+        if (position[0] >= len(self.landscape)):
+            return False
+
+        if (position[1] >= len(self.landscape[position[0]])):
+            return False
+
+        # If the obstacles can be "known" by the map
+        return not(self.landscape[position[0]][position[1]])
+
+    # On the problem, it seems that the obstacles can't be seen till the rover is already there
+    def is_there_obstacle(self):
+        not(self.landscape[self.position[0]][self.position[1]])
+
+    def is_position(self, position):
+        return position == self.position
+
+    def is_direction(self, direction):
+        return direction == self.direction
---
+++ MarsRover/test/mars_test.py
@@ -0,0 +1,134 @@
+import unittest
+
+from mars_rover import Rover, Direction, Move, Turn
+
+
+class Mars(unittest.TestCase):
+    """
+    Seems to not be able to use a BeforeAll to initiate the Kata
+    """
+
+    # This is the landscape, being 0,0 the top left position, and being 0 a place without obstacles. Both are 0 based
+    landscape = ((0, 0, 0, 1, 0, 0),
+                 (0, 0, 0, 0, 0, 0),
+                 (0, 1, 0, 1, 0, 0),
+                 (0, 0, 0, 0, 1, 1),
+                 (0, 0, 1, 0, 1, 0),
+                 (0, 0, 0, 1, 0, 0))
+
+    def test_can_init_with_given_values(self):
+        mars = Rover(self.landscape, (0, 0), Direction.N)
+        self.assertTrue(mars.is_direction(Direction.N))
+        self.assertTrue(mars.is_position((0, 0)))
+
+    def test_position_has_obstacles(self):
+        mars = Rover(self.landscape, (0, 0), Direction.N)
+        self.assertTrue(mars.can_move((4, 0)))
+        self.assertFalse(mars.can_move((0, 3)))
+
+    def test_cant_move_outside_boundaries(self):
+        mars = Rover(self.landscape, (0, 0), Direction.N)
+        self.assertFalse(mars.can_move((-4, 0)))
+        self.assertFalse(mars.can_move((0, 12)))
+        self.assertFalse(mars.can_move((11, 3)))
+        self.assertFalse(mars.can_move((4, -2)))
+
+    def test_turn_left(self):
+        mars = Rover(self.landscape, (0, 0), Direction.N)
+        mars.turn_left()
+        self.assertTrue(mars.is_direction(Direction.W))
+        mars.turn_left()
+        self.assertTrue(mars.is_direction(Direction.S))
+        mars.turn_left()
+        self.assertTrue(mars.is_direction(Direction.E))
+        mars.turn_left()
+        self.assertTrue(mars.is_direction(Direction.N))
+
+    def test_turn_right(self):
+        mars = Rover(self.landscape, (0, 0), Direction.N)
+        mars.turn_right()
+        self.assertTrue(mars.is_direction(Direction.E))
+        mars.turn_right()
+        self.assertTrue(mars.is_direction(Direction.S))
+        mars.turn_right()
+        self.assertTrue(mars.is_direction(Direction.W))
+        mars.turn_right()
+        self.assertTrue(mars.is_direction(Direction.N))
+
+    def test_move_forward_changes_position(self):
+        mars = Rover(self.landscape, (0, 0), Direction.E)
+        self.assertTrue(mars.move_forward())
+        self.assertTrue(mars.is_position((0, 1)))
+        self.assertTrue(mars.move_forward())
+        self.assertTrue(mars.is_position((0, 2)))
+
+    def test_move_backwards_changes_position(self):
+        mars = Rover(self.landscape, (0, 2), Direction.E)
+        self.assertTrue(mars.move_backwards())
+        self.assertTrue(mars.is_position((0, 1)))
+        self.assertTrue(mars.move_backwards())
+        self.assertTrue(mars.is_position((0, 0)))
+
+    def test_move_stops_on_edge_negative(self):
+        mars = Rover(self.landscape, (0, 0), Direction.E)
+        self.assertFalse(mars.move_backwards())
+        self.assertTrue(mars.is_position((0, 0)))
+
+        mars.turn_left()
+        self.assertTrue(mars.is_direction(Direction.N))
+        self.assertFalse(mars.move_forward())
+        self.assertTrue(mars.is_position((0, 0)))
+
+    def test_move_stops_on_edge_overflow(self):
+        mars = Rover(self.landscape, (5, 5), Direction.E)
+        self.assertFalse(mars.move_forward())
+        self.assertTrue(mars.is_position((5, 5)))
+
+        mars.turn_left()
+        self.assertTrue(mars.is_direction(Direction.N))
+        self.assertFalse(mars.move_backwards())
+        self.assertTrue(mars.is_position((5, 5)))
+
+    def test_move_stops_on_obstacle(self):
+        mars = Rover(self.landscape, (1, 3), Direction.N)
+        self.assertFalse(mars.move_forward())
+        self.assertTrue(mars.is_position((1, 3)))
+        self.assertFalse(mars.move_backwards())
+        self.assertTrue(mars.is_position((1, 3)))
+
+    def test_little_journey(self):
+        landscape = ((0, 0, 0, 1, 0, 0),
+                     (0, 1, 0, 0, 0, 0),
+                     (0, 0, 0, 1, 0, 0),
+                     (0, 0, 0, 0, 0, 1),
+                     (0, 0, 1, 1, 1, 0),
+                     (0, 0, 0, 0, 0, 0))
+
+        mars = Rover(landscape, (0, 0), Direction.E)
+
+        # We move two to the right, but the next one finds an obstacle
+        self.assertTrue(mars.move_forward())
+        self.assertTrue(mars.move_forward())
+        self.assertFalse(mars.move_forward())
+        
+        self.assertTrue(mars.is_direction(Direction.E))
+        self.assertTrue(mars.is_position((0,2)))
+
+        # We turn to the South and move three spaces, the next is an obstacle
+        mars.turn_right()
+        self.assertTrue(mars.move_forward())
+        self.assertTrue(mars.move_forward())
+        self.assertTrue(mars.move_forward())
+        self.assertFalse(mars.move_forward())
+
+        self.assertTrue(mars.is_direction(Direction.S))
+        self.assertTrue(mars.is_position((3,2)))
+
+        # We turn to the West and move backwards two spaces, the next is an obstacle
+        mars.turn_right()
+        self.assertTrue(mars.move_backwards())
+        self.assertTrue(mars.move_backwards())
+        self.assertFalse(mars.move_backwards())
+
+        self.assertTrue(mars.is_direction(Direction.W))
+        self.assertTrue(mars.is_position((3,4)))
