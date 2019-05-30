7f9ff667022aa2141951fc90250ee29b47e1d6fb
GinoGalotti <gino.galotti@gmail.com>
2019-05-30 17:12:45 +0200

    add tests

--- CardWar/card_war.py
+++ CardWar/card_war.py
@@ -1,5 +1,7 @@
 # Info and definition on https://github.com/gigasquid/wonderland-clojure-katas/tree/master/card-game-war
 
+# TODO: Fix the playing game
+
 from collections import namedtuple
 from random import shuffle
 
@@ -24,19 +26,101 @@ class CardValue:
     Queen = 12
     King = 13
 
+Card = namedtuple("Card", "Type Value")
+
 class CardGame:
 
-    def __init__(self):
-        self.card = namedtuple("Card", "Value Type")
+    def init_game(self):
         types = [CardType.Spades, CardType.Diamonds, CardType.Clubs, CardType.Hearts]
         values = [CardValue.Ace, CardValue.Two, CardValue.Three, CardValue.Four, CardValue.Five, CardValue.Six, 
             CardValue.Seven, CardValue.Eight, CardValue.Nine, CardValue.Ten, CardValue.Jack, CardValue.Queen, CardValue.King]
         deck = []
         for card_type in types:
             for card_value in values:
-                deck.append(self.card(card_value, card_type))
+                deck.append(Card(card_type, card_value))
         shuffle(deck)
 
         self.player1_deck = deck[:26]
         self.player2_deck = deck[26:]
+        self.cards_at_play = []
 
+    def play(self):
+        print("I'm playing " + str(self.player1_deck) +"+"+ str(self.player2_deck))
+        card1 = self.player1_deck.pop(0)
+        card2 = self.player2_deck.pop(0)
+        self.cards_at_play.extend([card1, card2])
+
+        result = self.wins(card1,card2)
+        print("Result is {0}".format(str(result)))
+
+        if result == 1:
+            self.player1_deck.extend(self.cards_at_play)
+            self.cards_at_play.clear()
+            print("Player 1 should win with {0}".format(str(self.player1_deck)))
+            return 1
+        
+        elif result == 2:
+            self.player2_deck.extend(self.cards_at_play)
+            self.cards_at_play.clear()
+            return 2
+        
+        else:
+            if self.is_end_game() == 0:
+                # Put 3 cards on the pile and play again
+                self.cards_at_play.append([self.player1_deck.pop(0)])
+                self.cards_at_play.append([self.player2_deck.pop(0)])
+                if self.is_end_game() != 0 :
+                    return 0
+                
+                self.cards_at_play.append([self.player1_deck.pop(0)])
+                self.cards_at_play.append([self.player2_deck.pop(0)])
+                if self.is_end_game() != 0:
+                    return 0
+
+                self.cards_at_play.append([self.player1_deck.pop(0)])
+                self.cards_at_play.append([self.player2_deck.pop(0)])
+                if self.is_end_game() != 0:
+                    return 0
+
+                return self.play()
+            else:
+                return 0
+        # Recursion, having a variable where you put the "bet"
+
+    def game(self):
+        winner = 0
+        while winner == 0:
+            self.play()
+            winner = self.is_end_game()
+        return winner
+
+    # 1 wins player 1, 2 wins player 2, 0 keep playing, 3 both ran out of cards at the same time
+    def is_end_game(self):
+        print(str(self.player1_deck) +"+"+ str(self.player2_deck))
+
+        if self.player1_deck == [] and self.player2_deck == []:
+            return 3
+        if self.player1_deck == []:
+            return 2
+        elif self.player2_deck == []:
+            return 1
+        return 0
+
+    # For testing purposes
+    def set_decks(self, deck1, deck2):
+        self.player1_deck = deck1
+        self.player2_deck = deck2
+        self.cards_at_play = []
+    
+    # Returns 0 when they are the same, 1 when the first one wins, 2 when the second one
+    def wins(self, card1, card2):
+        difference = card1.Value - card2.Value
+
+        if difference == 0:
+            return 0
+        if difference < 0: 
+            return 2
+        else:
+            return 1
+        
+        #TODO: Make option where Type matters and they have an entire deck each
--- CardWar/test/card_war_test.py
+++ CardWar/test/card_war_test.py
@@ -1,27 +1,101 @@
 import unittest
 
-from card_war import CardGame
+from card_war import CardGame, CardType, CardValue, Card
 
 
-class CardWardTest(unittest.TestCase):
+class CardWardUnitTest(unittest.TestCase):
     """
     Seems to not be able to use a BeforeAll to initiate the Kata
     """
 
     # Fail with one number
 
-    def test_two_equal(self):
-        self.assertEqual(recurring_character.recurring_character('AA'), 'A')
+    @classmethod
+    def setup_class(cls):
+        cls.card_game = CardGame()
+        cls.card_game.init_game()
 
-    def test_two_equal_and_jump(self):
-        self.assertEqual(recurring_character.recurring_character('ABAB'), 'A')
+    # Test setup works
+    def test_initial_deck(self):
+        self.assertEqual(26, len(self.card_game.player1_deck))
+        self.assertEqual(26, len(self.card_game.player2_deck))
 
-    def test_no_duplicate(self):
-        self.assertEqual(recurring_character.recurring_character('AB'), '')
+        # TODO: Check that we have one of each card
 
-    def test_empty_string(self):
-        self.assertEqual(recurring_character.recurring_character(''), '')
+    # Test internal methods
+    def test_ace_better_than_king(self):
+        self.assertEqual(1, self.card_game.wins(Card(CardType.Clubs, CardValue.Ace), Card(CardType.Diamonds, CardValue.King)))
+        self.assertEqual(2, self.card_game.wins(Card(CardType.Diamonds, CardValue.King), Card(CardType.Clubs, CardValue.Ace)))
 
-    def test_long_string(self):
-        self.assertEqual(
-            recurring_character.recurring_character('ACIDECALE'), 'C')
+    def test_jack_better_than_ten(self):
+        self.assertEqual(1, self.card_game.wins(Card(CardType.Clubs, CardValue.Jack), Card(CardType.Diamonds, CardValue.Ten)))
+        self.assertEqual(2, self.card_game.wins(Card(CardType.Diamonds, CardValue.Ten), Card(CardType.Clubs, CardValue.Jack)))
+    
+    def test_tie_when_same_value(self):
+        self.assertEqual(0, self.card_game.wins(Card(CardType.Clubs, CardValue.Ace), Card(CardType.Diamonds, CardValue.Ace)))
+        self.assertEqual(0, self.card_game.wins(Card(CardType.Diamonds, CardValue.Jack), Card(CardType.Clubs, CardValue.Jack)))
+
+    def test_is_end_game_is_0_when_both_have_decks(self):
+        self.assertEqual(0, self.card_game.is_end_game())
+    
+    def test_is_end_game_returns_correctly_when_one_is_empty(self):
+        new_game = CardGame()
+        new_deck = [Card(CardType.Clubs, CardValue.Ace), Card(CardType.Diamonds, CardValue.Ace)]
+
+        new_game.set_decks([], new_deck)
+        self.assertEqual(2, new_game.is_end_game())
+
+        new_game.set_decks(new_deck, [])
+        self.assertEqual(1, new_game.is_end_game())
+
+    def test_is_end_game_returns_correctly_when_both_are_empty(self):
+        new_game = CardGame()
+
+        new_game.set_decks([], [])
+        self.assertEqual(3, new_game.is_end_game())
+
+class CardWardPlayingTest(unittest.TestCase):
+    """
+    Seems to not be able to use a BeforeAll to initiate the Kata
+    """
+
+    # Test setup works
+    def test_playing_works(self):
+        card_game = CardGame()
+        card_game.init_game()
+        
+        self.assertNotEqual(0, card_game.game())
+
+    def test_play_first_card_win(self):
+        card_game = CardGame()
+        deck1 = [Card(CardType.Clubs, CardValue.Ace), Card(CardType.Diamonds, CardValue.Ace)]
+        deck2 = [Card(CardType.Clubs, CardValue.Queen), Card(CardType.Diamonds, CardValue.Queen)]
+        card_game.set_decks(deck1, deck2)
+
+        card_game.play()
+
+        self.assertEqual(3, len(card_game.player1_deck))
+        self.assertEqual(1, len(card_game.player2_deck))
+
+    def test_play_second_card_win(self):
+        card_game = CardGame()
+        deck1 = [Card(CardType.Clubs, CardValue.Ace), Card(CardType.Diamonds, CardValue.Ace), Card(CardType.Hearts, CardValue.Ace), Card(CardType.Spades, CardValue.Ace), Card(CardType.Spades, CardValue.King), Card(CardType.Spades, CardValue.Queen)]
+        deck2 = [Card(CardType.Clubs, CardValue.Ace), Card(CardType.Diamonds, CardValue.Queen), Card(CardType.Hearts, CardValue.Queen), Card(CardType.Spades, CardValue.Queen), Card(CardType.Spades, CardValue.Three), Card(CardType.Spades, CardValue.Two)]
+        card_game.set_decks(deck1, deck2)
+
+        card_game.play()
+
+        self.assertEqual(11, len(card_game.player1_deck))
+        self.assertEqual(1, len(card_game.player2_deck))
+
+    def test_loses_after_a_play(self):
+        card_game = CardGame()
+        deck1 = [Card(CardType.Clubs, CardValue.Ace)]
+        deck2 = [Card(CardType.Clubs, CardValue.Queen)]
+        card_game.set_decks(deck1, deck2)
+
+        card_game.play()
+        
+        self.assertEqual(2, len(card_game.player1_deck))
+        self.assertEqual(0, len(card_game.player2_deck))
+        self.assertEqual(1, card_game.is_end_game())
